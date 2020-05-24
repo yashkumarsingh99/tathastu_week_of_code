4754c58b9bebd7a9e94d6b947bac53be35db6336
yashkumarsingh99 <65813316+yashkumarsingh99@users.noreply.github.com>
2020-05-24 17:52:11 +0530

    Create Prog3.py

---
+++ day1/Prog3.py
@@ -0,0 +1,11 @@
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+print("Before Swaping")
+print("Value of a =", a)
+print("Value of b =", b)
+a = a + b
+b = a - b
+a = a - b
+print("After Swaping")
+print("Value of a =", a)
+print("Value of b =", b)
